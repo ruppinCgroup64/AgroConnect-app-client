9c432d9a7c73838c5c82362e27abe19095355dda
Adi Hadad <adihadad5566@gmail.com>
2024-05-17 17:45:24 +0300

    .

--- AgroConnect/src/api.js
+++ AgroConnect/src/api.js
@@ -1,14 +1,22 @@
 //API Templates
+<<<<<<< HEAD
 const BASE_URL = "https://proj.ruppin.ac.il/cgroup64/test2/tar1";
+=======
+let local = true;
+let BASE_URL = "https://proj.ruppin.ac.il/cgroup64/test2/";
+const port="7093"
+if (local) BASE_URL = "https://localhost:"+port; //השרת המקומי
+>>>>>>> Adi_fix
 
 export async function create(url, data) {
   try {
+    console.log(`${BASE_URL}/${url}`)
     let res = await fetch(`${BASE_URL}/${url}`, {
       method: "POST",
       body: data ? JSON.stringify(data) : "",
       headers: new Headers({
         "Content-type": "application/json; charset=UTF-8", //very important to add the 'charset=UTF-8'!!!!
-        Accept: "application/json; charset=UTF-8",
+        "Accept": "application/json; charset=UTF-8",
       }),
     });
     return await res.json(); //ככה נעשה
@@ -17,6 +25,7 @@ export async function create(url, data) {
     return {status:false, err}//בעיה בקוד/שגיאת שרת
   }
 }
+
 
 export async function read(url) {
   try {
