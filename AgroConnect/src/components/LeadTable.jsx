9989c6f1bd29262d64e126ed74be55bff76224fa
AmitDohan <36810106+AmitDohan@users.noreply.github.com>
2024-08-14 05:31:02 +0300

    ok.....?

--- AgroConnect/src/components/LeadTable.jsx
+++ AgroConnect/src/components/LeadTable.jsx
@@ -1,88 +1,45 @@
-import { View, StyleSheet, Text } from "react-native";
-import React, { useContext, useEffect, useState } from "react";
-import themeContext from "../theme/themeContex";
-import { Colors } from "../theme/color";
-import { TenderContext } from "../Context/TenderContext";
+import {
+  View, Text,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
+import React, { useState, useContext, Component } from 'react'
+import themeContext from '../theme/themeContex'
+import { useNavigation } from '@react-navigation/native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import { Colors } from '../theme/color'
 
-export default function LeadTable({ tenderId }) {
+
+export default function LeadTable() {
   const theme = useContext(themeContext);
-  const { getTendersBidsFarmer, TenderBidsFarm } = useContext(TenderContext);
-  const [createTableData, setcreateTableData] = useState([]);
-  const [bids, setBids] = useState([]);
+  const navigation = useNavigation();
+  const state = this.state;
+  
+  this.state = {
+      tableHead: ['מיקום', 'מחיר ליחידה',"מס' יחידות"],
+      tableData: [
+          ['1', '25₪', '4'],
+          ['2', '24₪', '2'],
+          ['3', '22₪', '1'],
+          ['4', '21₪', '2']
+      ]
+  }
 
-  const createArrData = (arr) => {
-    let matrix = [];
-    for (let i = 0; i < arr.length; i++) {
-      let item = [];
-      for (const [value] of Object.entries(arr[i])) {
-        item.push(value);
-      }
-      matrix.push(item);
-    }
-  };
-
-  const funcRead = async () => {
-    console.log("tenderId4444", tenderId);
-    let arr = await getTendersBidsFarmer(tenderId);
-    console.log('arr ==> ', arr)
-    arr.sort((a, b) => a.bidSortedNum - b.bidSortedNum)
-    setBids(arr);
-    console.log("arr", arr);
-  };
-  useEffect(() => {
-    funcRead();
-  }, []);
-
-  const BidTemplate = ({ bid }) => {
-    return (
-      <View style={[styles.row, styles.textData]}>
-        <Text>{bid.bidSortedNum}</Text>
-        <Text>{bid.consumerFirstName} {bid.consumerLastName}</Text>
-        <Text>{bid.consumerPhoneNum}</Text>
-        <Text>{bid.bidStatus.split(" ")[0] == "win" ? "כן" : "לא"}</Text>
-        <Text>{bid.unitPrice}</Text>
-        <Text>{bid.bidStatus.split(" ")[1] !== undefined ? bid.bidStatus.split(" ")[1] : bid.bidAmount}</Text>
-      </View>
-    );
-  };
+  
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.row, styles.textData, styles.head]}>
-        <Text>#</Text>
-        <Text>שם</Text>
-        <Text>טלפון</Text>
-        <Text>מנצח</Text>
-        <Text>מחיר למארז</Text>
-        <Text>כמות</Text>
+      <View style={styles.container}>
+          <Table borderStyle={{ borderWidth: 2, borderColor: Colors.primary }}>
+              <Row data={state.tableHead} style={styles.head} textStyle={styles.text} />
+              <Rows data={state.tableData} textStyle={styles.text} />
+          </Table>
       </View>
-      {/* Data */}
-      {bids.map((item) => <BidTemplate key={item.bidId} bid={item} />)}
-    </View>
-  );
-}
+  );//return
+
+}//Table
 
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 15, backgroundColor: "#fff" },
-  head: {
-    height: 40,
-    backgroundColor: Colors.primary,
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
-  row: {
-    flexDirection: "row",
-    backgroundColor: "#FFF",
-    padding: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc"
-  },
-  textData: { 
-    height: 40,
-    backgroundColor: Colors.secondary,
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
+  container: { flex: 1, padding: 16, paddingTop: 15, backgroundColor: '#fff' },
+  head: { height: 40, backgroundColor: Colors.primary }, 
+  text: { margin: 6, textAlign: 'center' }
 });
-
-
